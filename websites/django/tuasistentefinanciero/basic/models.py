3a7e97b5bf1bae0396c45ee27daaed961b231a0d
Cesar Orozco <cesar.orozco@hotmail.es>
2018-08-29 19:51:22 -0500

    Initial version of database model

--- websites/django/tuasistentefinanciero/basic/models.py
+++ websites/django/tuasistentefinanciero/basic/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+
 # Create your models here.
+class AppUser(models.Model):
+    """Usuario de la aplicación"""
+    name = models.CharField(verbose_name="User Name", max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class AppGoods(models.Model):
+    """Representa los bienes de un usuario"""
+    name = models.CharField(verbose_name="Description", max_length=150)
+    value = models.DecimalField(verbose_name="Good Value")
+
+    def __str__(self):
+        return self.name
+
+
+class AppDebts(models.Model):
+    """Representa las deudas de un usuario"""
+    name = models.CharField(verbose_name="Description",max_length=150)
+    value = models.DecimalField(verbose_name="Debt Value")
+
+    def __str__(self):
+        return self.name
+
+
+class AppCashbook(models.Model):
+    """Movimientos financieros"""
+    pass
+
+
+class AppCategory(models.Model):
+    """"""
+    pass
+
+
+class AppCategoryType(models.Model):
+    """Categorias: Ingresos o Gastos"""
+    name = models.CharField(verbose_name="Category Type Name",max_length=100)
+
+    def __str__(self):
+        return  self.name
+
+
+class AppFinancialStatement(models.Model):
+    """"""
+    pass
+
+class AppPaymentType(models.Model):
+    """"""
+    pass
